991dc2fb2204630b665b93d83cea3415a921c0c1
donsumayan <don.sumayan@gmail.com>
2017-10-05 13:58:40 +0800

    update server

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('Holy shit! This is awesome!');
+  res.json({'msg':'Holy shit! This is awesome!'});
 });
 
-app.listen(3000);
+app.listen(3000, function () {
+  console.log('=> redis endpoint active on port', 3000);
+});
